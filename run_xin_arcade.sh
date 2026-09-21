e1c14b5a1ae8474d033d4de395b093e7100bbeb8
Serge Honcharenko <serge.honcharenko@icloud.com>
2026-09-20 19:15:30 -0500

    add a 5 sec sleep after build

--- run_xin_arcade.sh
+++ run_xin_arcade.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")"
 docker compose build portal
 docker compose up -d --build --wait
 
+sleep 5 
+
 for port in 8080 8081 8082 8083 8084 8085 8091 8092 8093 8094 8095; do
   printf '%s  %s\n' "$port" "$(curl -s -o /dev/null -w '%{http_code}' "localhost:$port")"
 done
